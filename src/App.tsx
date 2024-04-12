c30dcc1071774748bc0a38a0ffe9292d30f1d911
Ferenandios <www.terunplay@gmail.com>
2024-04-13 00:39:34 +0300

    Added Button-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Image from "./components/Image/Image";
 import Inputs from "./components/Inputs/Inputs";
+import Button from "./components/Button/Button";
 
 const App: FC = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const App: FC = (): JSX.Element => {
         <div className="flex flex-col w-[500px] min-h-[250px] border-4 rounded-[30px] p-[16px] gap-[20px]">
           <Image />
           <Inputs />
+          <Button />
         </div>
       </section>
     </>

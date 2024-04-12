14f170b90063a55cf56f43fefb13b3af711f2f16
Ferenandios <www.terunplay@gmail.com>
2024-04-13 00:26:03 +0300

    Added one fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      robotoSlab: ["Roboto Slab", "sans-serif"],
+    },
   },
   plugins: [],
 };

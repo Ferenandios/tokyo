336c3a2193e77b19ca07e1718df3b11f3f75eb18
Ferenandios <www.terunplay@gmail.com>
2024-04-13 01:54:16 +0300

    Added Context Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Image from "./components/Image/Image";
 import Inputs from "./components/Inputs/Inputs";
 import Button from "./components/Button/Button";
+import { Context } from "./context/DataContext";
+import { IData } from "./types/types";
 
 const App: FC = (): JSX.Element => {
+  const [data, setData] = useState<IData>({
+    name: "",
+    phone: "",
+    email: "",
+    // ! COMPLETE CONTEXT TYPES FOR A FUNCTION
+    // ! COMPLETE CONTEXT TYPES FOR A FUNCTION
+    // ! COMPLETE CONTEXT TYPES FOR A FUNCTION
+    // ! COMPLETE CONTEXT TYPES FOR A FUNCTION
+    // ! COMPLETE CONTEXT TYPES FOR A FUNCTION
+  });
   return (
     <>
-      <section className="flex justify-center items-center w-screen h-screen">
-        <form
-          className="flex flex-col w-[500px] min-h-[250px] border-4 rounded-[30px] p-[16px] gap-[20px]"
-          name="mailTo"
-          method="post"
-        >
-          <Image />
-          <Inputs />
-          <Button />
-        </form>
-      </section>
+      <Context.Provider value={setData}>
+        <section className="flex justify-center items-center w-screen h-screen">
+          <form
+            className="flex flex-col w-[500px] min-h-[250px] border-4 rounded-[30px] p-[16px] gap-[20px]"
+            name="mailTo"
+            method="post"
+          >
+            <Image />
+            <Inputs />
+            <Button />
+          </form>
+        </section>
+      </Context.Provider>
     </>
   );
 };

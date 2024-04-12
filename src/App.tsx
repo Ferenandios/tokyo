3bc863704db6a19321c579135a1ee38f675696f3
Ferenandios <www.terunplay@gmail.com>
2024-04-12 22:59:46 +0300

    Created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { FC } from "react";
+import Image from "./components/Image/Image";
+import Inputs from "./components/Inputs/Inputs";
 
 const App: FC = (): JSX.Element => {
-  return <div className="bg-lime-200">works!</div>;
+  return (
+    <>
+      <section className="flex justify-center items-center w-screen h-screen">
+        <div className="w-[500px] min-h-[250px] bg-orange-300">
+          <Image />
+          <Inputs />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default App;

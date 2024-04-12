82b012ff81d19cfa53f5ce50ef639d6bb291226b
Ferenandios <www.terunplay@gmail.com>
2024-04-13 00:28:12 +0300

    Added styles for an Inner

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: FC = (): JSX.Element => {
   return (
     <>
       <section className="flex justify-center items-center w-screen h-screen">
-        <div className="w-[500px] min-h-[250px] bg-orange-300">
+        <div className="flex flex-col w-[500px] min-h-[250px] border-4 rounded-[30px] p-[16px] gap-[20px]">
           <Image />
           <Inputs />
         </div>

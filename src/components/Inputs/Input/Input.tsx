9449ce1be473dfa2876da3af5bd2e9f3816a6ec9
Ferenandios <www.terunplay@gmail.com>
2024-04-13 00:27:37 +0300

    Added styles for an Input-component

--- src/components/Inputs/Input/Input.tsx
+++ src/components/Inputs/Input/Input.tsx
@@ -4,8 +4,13 @@ import { type IInput } from "../../../types/types";
 const Input: FC<{ input: IInput }> = ({ input }): JSX.Element => {
   return (
     <>
-      <div>
-        <h3>{input.title}</h3>
+      <div className="flex flex-col w-full h-[60px]">
+        <h3 className="font-bold ml-[16px]">{input.title}</h3>
+        <input
+          className="w-full h-full border-2 rounded-full px-[16px]"
+          placeholder={input.placeholder}
+          type="text"
+        />
       </div>
     </>
   );

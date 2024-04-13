d510051b446e231513848ffd4b0d17e8aea6289e
Ferenandios <www.terunplay@gmail.com>
2024-04-13 21:47:18 +0300

    Fixed src for an image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import styles from "./Image.module.css";
+import ersh from "/ersh.jpg";
 
 const Image: FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.ansoc}>
-        <img className={styles.image} src="/ersh.jpg" alt="App Icon" />
+        <img className={styles.image} src={ersh} alt="App Icon" />
       </div>
     </>
   );

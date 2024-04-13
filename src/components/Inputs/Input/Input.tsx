3d52d17a5876164fc40eedc467bd7a20f2aa39dd
Ferenandios <www.terunplay@gmail.com>
2024-04-13 21:10:41 +0300

    Added handleChange

--- src/components/Inputs/Input/Input.tsx
+++ src/components/Inputs/Input/Input.tsx
@@ -1,12 +1,18 @@
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { type IInput } from "../../../types/types";
+import { Context } from "../../../App";
 
 const Input: FC<{ input: IInput }> = ({ input }): JSX.Element => {
+  const { setData } = useContext(Context);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setData({ type: input.type, value: event.target.value });
+  };
   return (
     <>
       <div className="flex flex-col w-full h-[60px]">
         <h3 className="font-bold ml-[16px]">{input.title}</h3>
         <input
+          onChange={handleChange}
           className="w-full h-full border-2 rounded-full px-[16px]"
           placeholder={input.placeholder}
           type="text"

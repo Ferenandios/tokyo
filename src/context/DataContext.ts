0b9eedeeea70fba5f3a65db5cd7d6c40653bb732
Ferenandios <www.terunplay@gmail.com>
2024-04-13 21:09:38 +0300

    Deleted hook for a context

--- src/context/DataContext.ts
+++
@@ -1,9 +0,0 @@
-import { type IData } from "./../types/types";
-import { createContext, useContext } from "react";
-
-export const Context = createContext<IData>({
-  name: "",
-  phone: "",
-  email: "",
-});
-export const useAppContext = () => useContext(Context);

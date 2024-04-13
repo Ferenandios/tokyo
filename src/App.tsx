6265b338782e71e4164e1de7484d432afbaeee27
Ferenandios <www.terunplay@gmail.com>
2024-04-13 21:29:22 +0300

    Added imported styles & comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { createContext, FC, useState } from "react";
+import styles from "./App.module.css";
 import Button from "./components/Button/Button";
 import Image from "./components/Image/Image";
 import Inputs from "./components/Inputs/Inputs";
 import { type IState } from "./types/types";
 
+// Creating Empty Context
 export const Context = createContext<IState>({
   data: { name: "", phone: "", email: "" },
   setData: () => {},
 });
 
 const App: FC = (): JSX.Element => {
+  // Creating State
   const [state, setState] = useState<IState>({
-    data: { name: "", phone: "", email: "" },
+    data: { name: "", phone: "", email: "" }, // default value of data
     setData: (payload) => {
+      // state.data[payload.type] = payload.value
       setState((prevState) => ({
         data: { ...prevState.data, [payload.type]: payload.value },
         setData: prevState.setData,
@@ -22,8 +26,8 @@ const App: FC = (): JSX.Element => {
   return (
     <>
       <Context.Provider value={state}>
-        <section className="flex justify-center items-center w-screen h-screen">
-          <div className="flex flex-col w-[500px] min-h-[250px] border-4 rounded-[30px] p-[16px] gap-[20px]">
+        <section className={styles.app}>
+          <div className={styles.inner}>
             <Image />
             <Inputs />
             <Button />
--- src/components/Inputs/Input/Input.tsx
+++ src/components/Inputs/Input/Input.tsx
@@ -1,19 +1,21 @@
 import { FC, useContext } from "react";
-import { type IInput } from "../../../types/types";
 import { Context } from "../../../App";
+import { type IInput } from "../../../types/types";
+import styles from "./Input.module.css";
 
 const Input: FC<{ input: IInput }> = ({ input }): JSX.Element => {
   const { setData } = useContext(Context);
+  // Function changes state.data[payload.type]
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setData({ type: input.type, value: event.target.value });
   };
   return (
     <>
-      <div className="flex flex-col w-full h-[60px]">
-        <h3 className="font-bold ml-[16px]">{input.title}</h3>
+      <div className={styles.inner}>
+        <h3 className={styles.title}>{input.title}</h3>
         <input
           onChange={handleChange}
-          className="w-full h-full border-2 rounded-full px-[16px]"
+          className={styles.input}
           placeholder={input.placeholder}
           type="text"
         />
